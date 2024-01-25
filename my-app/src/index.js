80dfe70129b52299188917fbf2385eed9ae538ea
Pantheleymon <pantheleymonbbx@yandex.ru>
2024-01-25 17:53:30 +0300

    Создание элементов

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,9 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+const elem = <h2>Hello World!</h2>;
+const elem2 = React.createElement('h2', null, 'Hello World! 2');
+const buttonText = 'Отправить';
+const elem3 = (
+  <div className="greet">
+    <form action="">
+    <input type="text" />
+    <button>{buttonText}</button>
+    </form>
+  </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  elem3
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
 );
