f162ac2bf4e2b74449b0d6622608c493cc2ca9a3
Pantheleymon <pantheleymonbbx@yandex.ru>
2024-01-25 20:04:03 +0300

    Верстка компонентов

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,5 +5,7 @@ import App from './components/app/app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App/>
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>
 );
