744b9fb1375ad8b9ba4554166e5a2fc55b0fce1a
Pantheleymon <pantheleymonbbx@yandex.ru>
2024-01-30 16:47:21 +0300

    Поиск сотрудников

--- my-app/src/components/search-panel/search-panel.js
+++ my-app/src/components/search-panel/search-panel.js
@@ -1,9 +1,30 @@
+import { Component } from 'react';
+
 import './search-panel.css';
 
-const SearchPanel = () => {
-    return (
-        <input placeholder="Найти сотрудника" type="text" className="form-control search-input"/>
-    );
+class SearchPanel extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            term: ''
+        }
+    }
+
+    onUpdateSearch = (e) => {
+        const term = e.target.value;
+        this.setState({term});
+        this.props.onUpdateSearch(term);
+    }
+
+    render() {
+        return (
+            <input placeholder="Найти сотрудника"
+                    type="text"
+                    className="form-control search-input"
+                    value={this.state.term}
+                    onChange={this.onUpdateSearch}/>
+        );
+    }
 }
 
 export default SearchPanel;
